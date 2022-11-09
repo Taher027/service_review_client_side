ee0f70c6eafea54948e251aff83716aac2cbd24c
Taher027 <abutaherh27@gmail.com>
2022-11-09 20:37:58 +0600

    added react-photo view component and disply full screen images

--- src/pages/AllServices.js
+++ src/pages/AllServices.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
+
+import 'react-photo-view/dist/react-photo-view.css';
 
 const AllServices = ({ service }) => {
     const { _id, img, price, name, description, rating } = service;
     return (
         <div className="card bg-base-100 text-white shadow-xl">
             <figure className="px-10 pt-10">
-                <img src={img} alt="Shoes" className="rounded-xl " />
+                <PhotoProvider>
+                    <div className="foo">
+
+                        <PhotoView src={img}>
+                            <img src={img} alt="Shoes" className="rounded-xl " />
+                        </PhotoView>
+
+                    </div>
+                </PhotoProvider>
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{name}</h2>
-                <p>Rating:{rating}</p>
-                <p>Price:{price}</p>
+                <p>Price: $ {price}</p>
                 <p>{description.slice(0, 100)}....</p>
+                <p>Rating:{rating}</p>
                 <div className="card-actions">
                     <Link to={`/services/${_id}`}>
                         <button className="btn btn-primary">View Details</button>
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const Service = () => {
     const service = useLoaderData();
@@ -7,10 +9,24 @@ const Service = () => {
     
     return (
         <div class="card lg:card-side bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Album" /></figure>
+            <figure>
+                <PhotoProvider>
+                    <div className="foo">
+
+                        <PhotoView src={img}>
+                            <img src={img} alt="Shoes" className="rounded-xl " />
+                        </PhotoView>
+
+                    </div>
+                </PhotoProvider>
+                
+            
+            </figure>
             <div class="card-body">
                 <h2 class="card-title">{name}</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <p>Price: ${price}</p>
+                <p>{description}</p>
+                <p>Rating: {rating}</p>
             </div>
         </div>
     );
