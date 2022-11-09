077bad40e238c4393647315c3b3a2f11df038b57
Taher027 <abutaherh27@gmail.com>
2022-11-09 19:32:19 +0600

    added single service card

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -3,11 +3,18 @@ import { useLoaderData } from 'react-router-dom';
 
 const Service = () => {
     const service = useLoaderData();
-    console.log(service)
+    const { img, name, description, rating, price } = service;
     
     return (
-        <div>
-            <h2> service:{service.name}</h2>
+        <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Album" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New album is released!</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Listen</button>
+                </div>
+            </div>
         </div>
     );
 };
