2f99ae7fb4988f2edad27d929ea134293405e0e1
Taher027 <abutaherh27@gmail.com>
2022-11-09 19:34:00 +0600

    added service card description

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -9,11 +9,8 @@ const Service = () => {
         <div class="card lg:card-side bg-base-100 shadow-xl">
             <figure><img src={img} alt="Album" /></figure>
             <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
+                <h2 class="card-title">{name}</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Listen</button>
-                </div>
             </div>
         </div>
     );
