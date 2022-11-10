424171147072fd1822bac042e8ebbe48db8b76e0
Taher027 <abutaherh27@gmail.com>
2022-11-10 08:10:40 +0600

    added contact section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             <HomeService> </HomeService>
             <div className='grid lg:grid-cols-2 mt-6'>
                 <div className="left">
-                    <img className='rounded p-5' src={photo} alt="img" />
+                    <img className=' p-5' src={photo} alt="img" />
                 </div>
                 <div className="right">
                     <h2 className='text-2xl text-white p-5'>Bringing out the best</h2>
@@ -20,10 +20,29 @@ const Home = () => {
                 </div>
             </div>
             <div className='grid lg:grid-cols-2'>
-                <div className="left">
-                    <h2>hello</h2>
+                <div className="mt-6">
+                    <h2 className='text-2xl text-white p-5'>Feel free to Contact Me</h2>
                 </div>
-                <div className="right"> <h2>bye</h2></div>
+                <div className="right">
+
+                    <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        <div className="mb-4">
+                            <label className="block text-white text-sm font-bold mb-2" for="username">
+                                Email
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email"/>
+                        </div>
+                        <div className="mb-6">
+                            <input className="shadow appearance-none border  rounded w-full py-2 px-3  mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="Your message"/>
+                            
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
 
